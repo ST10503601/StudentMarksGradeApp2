c766a940a79518b9ecd068150e4d77d9ad913b7d
ST10503601 <ST10503601@myemeris.edu.za>
2026-03-30 11:45:22 +0200

    change background to white

--- app/src/main/java/com/example/studentmarksgradeapp/MainActivity.kt
+++ app/src/main/java/com/example/studentmarksgradeapp/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.core.view.WindowInsetsCompat
 import java.util.jar.Attributes
 
 class MainActivity : AppCompatActivity() {
-    lateinit var txtStudentNo : EditText
-    lateinit var txtName : EditText
-    lateinit var txtAge : EditText
-    lateinit var txtSubject : EditText
-    lateinit var txtMark : EditText
-    lateinit var btnComputeGrade : Button
+    lateinit var txtStudentNo: EditText
+    lateinit var txtName: EditText
+    lateinit var txtAge: EditText
+    lateinit var txtSubject: EditText
+    lateinit var txtMark: EditText
+    lateinit var btnComputeGrade: Button
     lateinit var txtResults: TextView
 
     lateinit var btnClear: Button
@@ -25,58 +25,76 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
+        btnClear.setOnClickListener {
+            txtStudentNo.text.clear()
+            txtName.text.clear()
+            txtAge.text.clear()
+            txtSubject.text.clear()
+            txtMark.text.clear()
+            txtResults.text = ""
+        }
         btnComputeGrade.setOnClickListener {
             val StudentNo = txtStudentNo.text.toString()
             val Name = txtName.text.toString()
             val Age = txtAge.text.toString()
-            val Suject = txtSubject.text.toString()
-            val Mark= txtMark.text.toString()
+            val Subject = txtSubject.text.toString()
+            val Mark = txtMark.text.toString()
             val Results = txtResults.text.toString()
 
-            btnClear.setOnClickListener{
-                txtStudentNo.text.clear()
-                txtName.text.clear()
-                txtAge.text.clear()
-                txtSubject.text.clear()
-                txtMark.text.clear()
-                txtResults.text=""
-            }
 
-         // Check all fields present
-        if (StudentNo.isEmpty()|| Name.isEmpty()|| AgeText.isEmpty()||Subject.isEmpty||MarkText.isEmpty()) {
-        }
-            txtResults.text = "Please fill all fields"
-        }else{
-            // Validate Age
-            val age = ageText.toIntOrNull()
-        }   if(age==null||age<=0){
-            txtResults.text="Enter a valid Age"
-        }else{
-        //Validate mark
-        val mark =MarkText.toDoubleOrNull()
-            if(mark==null||mark<0.0||mark>100.0){
-                txtResults.text="Enter a valid mark between 0 and 100"
-            }else{
-                //Compute grade using if/else-if inline
-                val grade: String
-                if(mark>=90.0){
-                    grade="A"
-                }else if(mark>80.0){
-                    grade="B"
-                }else if (mark >=70){
-                    grade="C"
-                }else if (mark >=60.0){
-                    grade="D"
-                }else if (mark >=50.0){
+            // Check all fields present
+            if (StudentNo.isEmpty() || Name.isEmpty() || Age.isEmpty() || Subject.isEmpty() || Mark.isEmpty()) {
+
+                txtResults.text = "Please fill all fields"
+            } else {
+
+                // Validate Age
+                val age = Age.toIntOrNull()
+                if (age == null || age <= 0) {
+                    txtResults.text = "Enter a valid Age"
+                } else {
+                    //Validate mark
+                    val mark = Mark.toDoubleOrNull()
+                    if (mark == null || mark < 0.0 || mark > 100.0) {
+                        txtResults.text = "Enter a valid mark between 0 and 100"
+                    } else {
+                        //Compute grade using if/else-if inline
+                        val grade: String
+                        if (mark >= 90.0) {
+                            grade = "A"
+                        } else if (mark > 80.0) {
+                            grade = "B"
+                        } else if (mark >= 70) {
+                            grade = "C"
+                        } else if (mark >= 60.0) {
+                            grade = "D"
+                        } else if (mark >= 50.0) {
+                            grade = "E"
+                        } else if (mark >= 40.00) {
+                            grade = "F"
+                        }
+                        txtResults.text =
+                            "Student No.:" + StudentNo + "\n" +
+                                    "Name:" + Name + "\n" +
+                                    "Age:" + age + "\n" +
+                                    "Subject" + Subject + "\n" +
+                                    "Mark obtained:" + mark + "\n" + "Grade:" + mark
+
+                    }
                 }
+
+
+                ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.txtStudentNo)) { v, insets ->
+                    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                    v.setPadding(
+                        systemBars.left,
+                        systemBars.top,
+                        systemBars.right,
+                        systemBars.bottom
+                    )
+                    insets
                 }
             }
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.txtStudentNo)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
     }
 }
